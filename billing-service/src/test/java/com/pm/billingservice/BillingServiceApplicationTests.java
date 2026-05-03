7816c45a03e9f3c50374df7344807e99246edc55
shreyas2804 <shreyasmarkad2000@gmail.com>
2026-05-03 16:37:39 +0530

    billing service changes

---
+++ billing-service/src/test/java/com/pm/billingservice/BillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.billingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingServiceApplicationTests {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BillingServiceApplication.class, args);
+  }
+
+}
